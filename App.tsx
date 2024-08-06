c21cb8e16b79f1ba1a5e639a0bfd0b9ab1fa924f
snow1032 <jacksonpoon1032@gmail.com>
2024-08-06 23:08:34 +0800

    create_music_app

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import MainContainer from './src/navigation/MainContainer';
+
+function App() {
+  return (
+    <MainContainer />
+  );
+}
+export default App;
+

e063276f8f1d9592a2990438b80e3f289c30755e
snow1032 <jacksonpoon1032@gmail.com>
2024-08-07 20:00:23 +0800

    add slider

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,27 @@
 import * as React from 'react';
 import { View, Text, Button } from 'react-native';
+import {SliderBox} from 'react-native-image-slider-box';
 
 export default function HomeScreen({ navigation }) {
+    const images =[
+        require('../assets/images/model1.jpg'),
+        require('../assets/images/model2.jpg'),
+        require('../assets/images/model3.jpg'),
+        require('../assets/images/model4.jpg'),
+        require('../assets/images/model5.jpg'),
+      ]
     return (
 
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text onPress={() => Alert('This is the "Home" screen.')}>HomeScreen</Text>
+        <View>
+            {/* <Text onPress={() => Alert('This is the "Home" screen.')}>HomeScreen</Text> */}
+            <SliderBox images={images}  dotColor="red" inactiveDotColor="black" 
+            dotStyle={{width: 10, height: 10, borderRadius: 50}}
+            imageLoadingColor="black"
+            autoplay={true}
+            autoplayInterval={3000}
+            circleLoop={true}
+            onCurrentImagePressed={(index) => alert(index+1) }
+            />
         </View>
     );
 }
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -4,10 +4,12 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 
 export default function SettingsScreen({ navigation }) {
+ 
     return (
 
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text onPress={() => navigation.navigate("Home")}>Settings Screen</Text>
+         
         </View>
     );
 }
